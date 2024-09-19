a151a9af55f6050398f25a26612bf49dfe0355b4
Sanusi Sheriff <sanusisheriff16@gmail.com>
2024-09-19 18:35:28 +0100

    only shake

---
+++ mylibrary/src/main/java/com/sanusi/mylibrary/ShakeDetector.kt
@@ -0,0 +1,53 @@
+package com.sanusi.mylibrary
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import kotlin.math.sqrt
+
+class ShakeDetector(
+    private val context: Context,
+    private var onShake: () -> Unit
+) : SensorEventListener {
+
+   // private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val shakeThreshold = 8.0f
+    private var lastShakeTime = 0L
+
+    fun start() {
+//        sensorManager.registerListener(
+//            this,
+//            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+//            SensorManager.SENSOR_DELAY_UI
+//        )
+    }
+
+    fun stop() {
+       // sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+//        val x = event.values[0]
+//                val y = event.values[1]
+//                val z = event.values[2]
+//
+//                val gForce = sqrt(x * x + y * y + z * z) / SensorManager.GRAVITY_EARTH
+//
+//                if (gForce > shakeThreshold) {
+//                    val currentTime = System.currentTimeMillis()
+//
+//                    // Only react if the shake happens after 500ms to avoid multiple triggers
+//                    if (currentTime - lastShakeTime > 500) {
+//                        lastShakeTime = currentTime
+//                        onShake()
+//                    }
+//                }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Do nothing
+    }
+}
+
